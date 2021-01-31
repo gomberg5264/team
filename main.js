de03bc54b65911c8b0537bb9bbda642726bfb7ce
gomberg5264 <61816796+gomberg5264@users.noreply.github.com>
2021-01-31 00:42:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fadeEffect = setInterval(() => {
     // if we don't set opacity 1 in CSS, then
     // it will be equaled to "" -- that's why
     // we check it, and if so, set opacity to 1
-    if (loader.style.opacity) {
+    if (!loader.style.opacity) {
       loader.style.opacity = 1;
     }
     if (loader.style.opacity > 0) {
@@ -13,3 +13,4 @@ const fadeEffect = setInterval(() => {
       clearInterval(fadeEffect);
     }
   },150);
+
