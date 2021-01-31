9a08a649210ccf8f70a2aeb77359916746cc911b
gomberg5264 <61816796+gomberg5264@users.noreply.github.com>
2021-01-30 22:17:38 -0500

    premerge

---
+++ main.js
@@ -0,0 +1,15 @@
+const loader = document.getElementById('loader');
+console.log(loader)
+const fadeEffect = setInterval(() => {
+    // if we don't set opacity 1 in CSS, then
+    // it will be equaled to "" -- that's why
+    // we check it, and if so, set opacity to 1
+    if (!loader.style.opacity) {
+      loader.style.opacity = 1;
+    }
+    if (loader.style.opacity > 0) {
+      loader.style.opacity -= 0.075;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  },150);
